21f6dd92bf647736eb6cd8b62174e7c45271c483
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-07-23 01:18:37 -0600

    [First] commit

---
+++ js/index.js
@@ -0,0 +1,71 @@
+let albumes = []
+let desplegado;
+document.querySelector(".mas").addEventListener("click", mas)
+
+function mas() {
+	const nombre = document.querySelector("#album").value.trim()
+	if (nombre) {
+		albumes.push({ album: nombre, imagenes: [] })
+		escribir();
+	}
+}
+
+function escribir() {
+	document.querySelector(".albumes").innerHTML = ""
+	albumes.map((valor, indice) => {
+		document.querySelector(".albumes").insertAdjacentHTML("beforeend", `
+		<div class="cadaAlbum" onclick="desplegar(this)" jab="${indice}">
+			<div class="nombreAlbum"
+			contenteditable="true"
+			onfocus="activar(this)"
+			onblur="desactivar(this)"
+			onkeydown="detectarEnter(this, event)">
+				${valor.album}
+			</div>
+		</div>
+		`)
+	})
+}
+
+function activar() { }
+
+function desactivar() { }
+
+function detectarEnter() { }
+
+function desplegar(yo) {
+	document.querySelector(".miAlbum").style.display = "block"
+	desplegado = (yo.getAttribute("jab"))
+	const datos = albumes[desplegado];
+	document.querySelector(".miAlbum").innerHTML = `
+		<h1>${datos.album}</h1>
+		<div class="imagenes"></div>
+		<div class="caja">
+			<input type="file" name="fichero" id="fichero" accept="image/*"/>
+			<button id="enviar" onclick="enviar()">Subir fichero</button>
+		</div> `
+	if (datos.imagenes.length > 0) {
+		datos.imagenes.map((valor) => {
+			// Dibuje las imagenes
+		})
+	}
+}
+
+function enviar() {
+	if (fichero.files.length > 0) {
+		let data = new FormData()
+		data.append('fichero', fichero.files[0])
+		fetch('php/subir.php', {
+			method: 'POST',
+			body: data
+		})
+			.then(response => response.text())
+			.then(data => {
+				albumes[desplegado].imagenes.push(data.trim())
+				codigoHTML(data)
+				escribir()
+			})
+	}
+}
+
+function codigoHTML() { }
---
+++ php/subir.php
@@ -0,0 +1,28 @@
+<?php
+
+$carpeta = "../ficheros/";
+$nombreOriginal = basename($_FILES["fichero"]["name"]);
+$uploadOK = 1;
+$formatoImagen = strtolower(pathinfo($nombreOriginal, PATHINFO_EXTENSION));
+$nombreUnico = md5(uniqid());
+$nombreFinal = $carpeta . $nombreUnico . "." . $formatoImagen;
+$imagenAMostrar = "fichero/" . $nombreUnico . "." . $formatoImagen;
+
+$check = getimagesize($_FILES["fichero"]["tmp_name"]);
+
+if($check === false){
+	echo "El archivo no es una imagen. Lo siento, únicamente se pueden subir imágenes.";
+	$uploadOK = 0;
+}
+if($_FILES["fichero"]["size"] > 10000000) {
+	echo "Lo siento, tu archivo es demasiadogrande. El tamaño máximo es de 10MB.";
+	$uploadOK = 0;
+}
+if ($uploadOK === 1) {
+	if(move_uploaded_file($_FILES["fichero"]["tmp_name"],$nombreFinal)){
+		echo $imagenAMostrar;
+	} else {
+		echo "Lo siento, ha habido un error al subir tu archivo.";
+	}
+}
+?>
