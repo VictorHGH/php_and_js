2680162e0dc11d129c45aee81cc37d8316808e5f
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2024-07-24 01:04:42 -0600

    [New] script in php

---
+++ php/borrarFichero.php
@@ -0,0 +1,11 @@
+<?php
+if (isset($_POST['ficheroABorrar'])) {
+	$nombre_del_archivo = "../".trim($_POST['ficheroABorrar']);
+	if (file_exists($nombre_del_archivo)) {
+		unlink($nombre_del_archivo);
+		echo "El fichero $nombre_del_archivo ha sido borrado";
+	} else {
+		echo "El fichero $nombre_del_archivo no existe";
+	}
+}
+?>
